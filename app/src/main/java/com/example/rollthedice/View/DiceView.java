d53e3214d4c516865151bbeaa9ae1d6c292e8ae5
merillas <adrianmpgg@gmail.com>
2024-01-08 18:39:31 +0100

    Commit Merillas fondo con color rotatorio
    Se añade funcionalidad al fondo del DiceView para que rote entre nuestros 5 colores claves
    
    Navegacion:
    -toda
    
    Xml:
    -Main
    -Question
    -Transition

--- app/src/main/java/com/example/rollthedice/View/DiceView.java
+++ app/src/main/java/com/example/rollthedice/View/DiceView.java
@@ -1,12 +1,19 @@
 package com.example.rollthedice.View;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.rollthedice.R;
 import com.example.rollthedice.Router.Router;
 
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,10 +22,12 @@ import java.util.Random;
 
 public class DiceView extends AppCompatActivity {
 
-
+    private int[] coloresResources = {R.color.yellowMitology, R.color.redFood, R.color.greenNature, R.color.purpleTecnology, R.color.orangeTrips};
+    private int duracionTransicion = 5000;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.diceview);
+        cambiarColorDeFondo();
 
         Button diceButton = findViewById(R.id.diceButtonDiceView);
 
@@ -32,6 +41,30 @@ public class DiceView extends AppCompatActivity {
             }
         });
     }
+
+
+
+    public void cambiarColorDeFondo() {
+        final ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), ContextCompat.getColor(this, coloresResources[0]),
+                ContextCompat.getColor(this, coloresResources[1]),
+                ContextCompat.getColor(this, coloresResources[2]),
+                ContextCompat.getColor(this, coloresResources[3]),
+                ContextCompat.getColor(this, coloresResources[4]));
+
+        colorAnimation.setDuration(duracionTransicion);
+        colorAnimation.setRepeatCount(ValueAnimator.INFINITE);
+        colorAnimation.setRepeatMode(ValueAnimator.REVERSE);
+
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                getWindow().getDecorView().setBackgroundColor((int) animator.getAnimatedValue());
+            }
+        });
+
+        colorAnimation.start();
+    }
+
     public int generarNumeroAleatorio() {
         Random random = new Random();
         int randomNumber = random.nextInt(5);
