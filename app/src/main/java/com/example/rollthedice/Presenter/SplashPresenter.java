6632546b1d01452a5cf5c1d9889f229a773c4d4a
Alvaro <alvaroguadalupe02rgmail.com>
2024-02-02 12:44:01 +0100

    Splash creado,con gift(necesita un cambio)

--- app/src/main/java/com/example/rollthedice/Presenter/SplashPresenter.java
+++ app/src/main/java/com/example/rollthedice/Presenter/SplashPresenter.java
@@ -1,29 +1,27 @@
 package com.example.rollthedice.Presenter;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rollthedice.View.LoginView;
-import com.example.rollthedice.View.SplashView;
 
 public class SplashPresenter extends AppCompatActivity {
 
-    protected void transition(Context context) {
+    protected void transition() {
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(context, LoginView.class);
+                Intent intent = new Intent(SplashPresenter.this, LoginView.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
 
             }
-        }, 5000);
+        }, 12000);
 
 
     }
--- app/src/main/java/com/example/rollthedice/View/SplashView.java
+++ app/src/main/java/com/example/rollthedice/View/SplashView.java
@@ -1,11 +1,15 @@
 package com.example.rollthedice.View;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.example.rollthedice.Presenter.SplashPresenter;
 import com.example.rollthedice.R;
 
@@ -16,10 +20,46 @@ public class SplashView extends SplashPresenter {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashview);
 
-        transition(this);
 
+
+        ImageView mSplash = findViewById(R.id.fondo);
+      /*  Glide.with(this)
+                .load(R.drawable.splashgiff)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.orangeTrips)))
+                .into(mSplash);*/
+
+        try {
+            Glide.with(this)
+                    .load(R.drawable.splashgiff)
+                    .into(mSplash);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        transition();
 
     }
 
 
 }
+
+
+/* super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ImageView rayo = findViewById(R.id.logo);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.blink);
+        rayo.startAnimation(animation);
+        ImageView mSplash = findViewById(R.id.fondo);
+
+
+        Glide.with(this)
+                .load(R.drawable.fondo_mar)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_700)))
+                .into(mSplash);
+        openApp();
+*/
